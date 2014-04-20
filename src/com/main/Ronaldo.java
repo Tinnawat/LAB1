d21bdf6ab1f2148fe77f1557d2bdd9ec96a218ea
Tinnawat <Tinnawat.tinnawat@gmail.com>
2014-04-20 14:10:47 +0700

    Java Basic Lab1

---
+++ src/com/main/Ronaldo.java
@@ -0,0 +1,26 @@
+package com.main;
+
+public class Ronaldo {
+	String name;
+	 String position;
+	 String[]  fiiendPlayer;
+	
+	public Ronaldo(){
+		name = "Ronaldo";
+		position = "Striker";
+		fiiendPlayer = new String[]{"Owen","Rooney","Kaka"};
+
+	}
+	public Ronaldo(int i){
+		}
+	public String getName(){
+		return name;
+	}
+	public String getposition(){
+		return position;
+	}
+	public String[] fiiendPlayer(){
+		return fiiendPlayer;
+	}
+
+}
---
+++ src/com/main/Run.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Run {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ronaldo kk = new Ronaldo();
+		System.out.println("name : "+kk.getName());
+		System.out.println("position : "+kk.getposition());
+		System.out.println("friendPlayer : "+kk.fiiendPlayer()[1]);
+
+	}
+
+}
